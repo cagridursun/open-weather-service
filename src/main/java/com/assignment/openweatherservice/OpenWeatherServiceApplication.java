84d0c65bcd8cc1e1a41a016e87c68c572a32d969
cagridursun <cagridursun@gmail.com>
2023-03-09 02:35:20 +0100

    production-grade solution

---
+++ src/main/java/com/assignment/openweatherservice/OpenWeatherServiceApplication.java
@@ -0,0 +1,35 @@
+package com.assignment.openweatherservice;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.module.paramnames.ParameterNamesModule;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+import java.time.Clock;
+
+@SpringBootApplication
+@EnableCaching
+@EnableScheduling
+public class OpenWeatherServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OpenWeatherServiceApplication.class, args);
+    }
+
+    @Bean
+    public ObjectMapper objectMapper() {
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new ParameterNamesModule());
+
+        return objectMapper;
+    }
+
+    @Bean
+    public Clock clock() {
+        return Clock.systemDefaultZone();
+    }
+
+}
